b9e9ca61c42214b9661e0ba0459ff74f9b92f733
Franco <otta@itsstudiosaurus.com>
2024-05-16 20:50:48 -0300

    2nd part completed - ui Desing

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Conversation = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+        <div className="avatar onine">
+          <div className="w-12 rounded-full">
+            <img
+              src="https://images.freeimages.com/fic/images/icons/560/fast_icon_users/128/user.png"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">John doe</p>
+            <span className="text-xl">🎃</span>
+          </div>
+        </div>
+      </div>
+      <div className="divider my-0 py-0 h-1" />
+    </>
+  );
+};
+
+export default Conversation;
